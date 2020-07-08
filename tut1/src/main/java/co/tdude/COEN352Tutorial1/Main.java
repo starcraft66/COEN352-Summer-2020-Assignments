95de0f262e4f0dcc261ce4283080e7ab90ae050a
Tristan Gosselin-Hane <starcraft66@gmail.com>
2020-07-07 23:09:40 -0400

    Finish tutorial 1

--- tut1/src/main/java/co/tdude/COEN352Tutorial1/Main.java
+++ tut1/src/main/java/co/tdude/COEN352Tutorial1/Main.java
@@ -1,5 +1,9 @@
 package co.tdude.COEN352Tutorial1;
 
+import com.xeiam.xchart.Chart;
+import com.xeiam.xchart.QuickChart;
+import com.xeiam.xchart.SwingWrapper;
+
 import java.math.BigInteger;
 import java.time.Duration;
 import java.time.Instant;
@@ -8,12 +12,29 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the factorial to calculate: ");
+        System.out.print("Enter up to which factorial to calculate: ");
         int factorial = scanner.nextInt();
+        double[] times = new double[factorial];
+        double[] xData = new double[factorial];
+
+        for (int i = 1; i <= factorial; i++) {
+            xData[i - 1] = i;
+        }
+
+        for (int i = 1; i <= factorial; i++) {
+            times[i - 1] = (double) timeFactorial(BigInteger.valueOf(i));
+        }
+
+        Chart chart = QuickChart.getChart("Factorial execution time (ns)", "Factorial", "Execution time (ns)", "Factorial execution time (ns)", xData, times);
+        new SwingWrapper(chart).displayChart();
+    }
+
+    public static long timeFactorial(BigInteger n) {
         Instant start = Instant.now();
-        System.out.println(factorial(BigInteger.valueOf(factorial)));
+        System.out.println("Iteration " + n);
+        factorial(n);
         Instant finish = Instant.now();
-        System.out.println("It took " + Duration.between(start, finish).toMillis() + " ms!");
+        return Duration.between(start, finish).toMillis();
     }
 
     public static BigInteger factorial(BigInteger n){
